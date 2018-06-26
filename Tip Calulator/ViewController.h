28bccc9f87c70053db35c6c7693fdd67f1aadedb
Katie Kwan <katiekwan@fb.com>
2018-06-26 13:20:56 -0700

    Initial Commit

---
+++ Tip Calulator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tip Calulator
+//
+//  Created by Katie Kwan on 6/26/18.
+//  Copyright © 2018 Katie Kwan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
